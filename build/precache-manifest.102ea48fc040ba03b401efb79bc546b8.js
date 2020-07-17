4ca4ae322d0d77c1202cef959e7f7c3c9cab42d9
unknown <qgieng@uci.edu>
2020-07-17 03:10:41 -0700

    error handling/note: error handling should go at end

---
+++ build/precache-manifest.102ea48fc040ba03b401efb79bc546b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b88d9991addf4e8882d2da8715c8cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2c6d4a8bc17087463c",
+    "url": "/static/css/main.07b9ecc9.chunk.css"
+  },
+  {
+    "revision": "f805d050fdda8c90bfab",
+    "url": "/static/js/2.af31c445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.af31c445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2c6d4a8bc17087463c",
+    "url": "/static/js/main.935176f1.chunk.js"
+  },
+  {
+    "revision": "789fccc14e93a94694aa",
+    "url": "/static/js/runtime-main.ea55758b.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express')
-
 const app = express()
-const mongoose = require('mongoose');
+
 const cors = require('cors')
 const Note = require('./models/notes');
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+
+/** 
 let notes = [
   {
     id: 1,
@@ -29,7 +31,7 @@ let notes = [
     important: true
   }
 ]
-
+*/
 
 app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes=>{
@@ -37,12 +39,30 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
+
+
+
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
   return maxId + 1
 }
+
+app.put('/api/notes/:id', (request,response,next)=>{
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, {new:true})
+  .then(updatedNote=>{
+    response.json(updatedNote);
+  })
+  .catch(error=>next(error));
+});
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -66,16 +86,49 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note=>{
-    response.json(note)});
+  Note.findById(request.params.id)
+    .then(note=>{
+      if(note){
+        response.json(note);
+      }
+      else{
+        response.status(404).end();
+      }
+    })
+    .catch(error=>{
+      //console.log("catch error for api/notes/id " , error);
+      //response.status(400).send({error:'malformatted id'});
+      next(error);
+    })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result=>{
+      response.status(204).end();
+    })
+    .catch(error=>next(error));
 })
+
+
+
+
+/*----------------------------------------------------*/
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 
