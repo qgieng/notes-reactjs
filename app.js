b20010d12b1cf7d7876475b0a9ea13a5be9fd2de
unknown <qgieng@uci.edu>
2020-07-19 19:41:50 -0700

    project restructure and working changes

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ logger.info('connecting to', config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error('error connection to MongoDB:', error.message);
+  });
 
 app.use(cors());
 app.use(express.static('build'));
--- models/note.js
+++ models/note.js
@@ -1,17 +1,6 @@
-const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator')
-mongoose.set('useFindAndModify', false)
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+mongoose.set('useFindAndModify', false);
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -19,19 +8,19 @@ const noteSchema = new mongoose.Schema({
     required: true,
     minlength: 5
   },
-  date: Date,
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
-})
-
+});
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-noteSchema.plugin(uniqueValidator)
-
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Note', noteSchema);
