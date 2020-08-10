3eccea747d53e048684041bc96cb58fe50ad943e
unknown <qgieng@uci.edu>
2020-08-10 00:30:40 -0700

    user admin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
+const userRouter = require('./controllers/users')
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -23,7 +24,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
-
+app.use('/api/usrs', userRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
