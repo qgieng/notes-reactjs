46c4912074a59fa3eb3d9dc494e073130792d0a0
unknown <qgieng@uci.edu>
2020-08-23 03:43:59 -0700

    finish some testing

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,12 +8,11 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])  
-  await noteObject.save()
+  console.log('cleared')
+  
+  const noteObjects = helper.initialNotes.map(note => new Note(note));
+  const promiseArray = noteObjects.map(note => note.save());
+  await Promise.all(promiseArray);
 })
 
 test('notes are returned as json', async () => {
