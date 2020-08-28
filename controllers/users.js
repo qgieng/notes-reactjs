39b9b595a69ef721ded9937a50c5cff31b7692fe
unknown <qgieng@uci.edu>
2020-08-28 03:59:15 -0700

    finished with notes

--- controllers/users.js
+++ controllers/users.js
@@ -2,19 +2,11 @@ const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
 
-const getTokenFrom = request =>{
-    const authorization = request.get('authorization');
-    if(authorization && authorization.toLowerCase().startsWith('bearer ')){
-        return authorization.substring(7);
-    }
-    return null;
-}
+
 
 usersRouter.post('/', async (request, response)=>{
     const body = request.body
-    const token = getTokenFrom(request);
-
-    const decodeToken  jwt.verify(token, process.env.SECRET);
+    
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
